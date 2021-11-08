286a3b994783bf793694521a76660541d5901d5a
Adelin <adebota02@gmail.com>
2021-11-08 11:48:20 +0100

    Ventana profesor

---
+++ TeamMaker/Paginas usuario/Profesor.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Login/Login.css">
+    <title>Profesor</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <header>
+        <div id="img_header0"></div>
+        <div id="img_header1"></div>
+        <div id="img_header2"></div>
+        <div id="img_header3"></div>
+        <div id="img_header4"></div>
+        <div id="img_header5"></div>
+        <div id="img_header6"></div>
+        <div id="img_header7"></div>
+        <div id="img_header8"></div>
+    </header>
+
+    <main>
+        <h1>Bienvenido Usuario</h1>
+
+        <input type="button" value="Gestionar alumnos">
+        <input type="button" value="Gestionar grupos">
+        <input type="button" value="Ver respuestas">
+        <input type="button" value="Equipos sugeridos">
+        <input type="button" value="Salir">
+
+    </main>
+
+    <footer>
+        <div id="img_footer0"></div>
+        <div id="img_footer1"></div>
+        <div id="img_footer2"></div>
+        <div id="img_footer3"></div>
+        <div id="img_footer4"></div>
+        <div id="img_footer5"></div>
+    </footer>
+
+</body>
+</html>
