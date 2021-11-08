1310e2cb388405548d2898ca7d25d22e91b53f42
Adelin <adebota02@gmail.com>
2021-11-08 14:21:33 +0100

    Paginas gestiones creadas

---
+++ TeamMaker/Gestiones/GestionarAdmin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Login/Login.css">
+    <title>Gestionar Administrador</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <header>
+        <div id="img_header0"></div>
+        <div id="img_header1"></div>
+        <div id="img_header2"></div>
+        <div id="img_header3"></div>
+        <div id="img_header4"></div>
+        <div id="img_header5"></div>
+        <div id="img_header6"></div>
+        <div id="img_header7"></div>
+        <div id="img_header8"></div>
+    </header>
+
+    <main>
+
+        <input type="button" value="Crear administrador" class="Gadmin"> <br>
+        <input type="button" value="Modificar administrador" class="Gadmin"> <br>
+        <input type="button" value="Eliminar administrador" class="Gadmin"> <br>
+        <input type="button" value="Salir" class="GadminSalir"> <br>
+
+    </main>
+
+    <footer>
+        <div id="img_footer0"></div>
+        <div id="img_footer1"></div>
+        <div id="img_footer2"></div>
+        <div id="img_footer3"></div>
+        <div id="img_footer4"></div>
+        <div id="img_footer5"></div>
+    </footer>
+
+</body>
+</html>
