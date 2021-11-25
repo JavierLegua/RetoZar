7a7b65f064146031f589b4c0e4b9dafc6663e3a5
Faced <angelfaced@hotmail.es>
2021-11-25 10:03:15 +0100

    Adelin deja de tocar los huevos con el filezilla

--- TeamMaker/Grupos/MostrarResultadosGrupo.php
+++ TeamMaker/Grupos/MostrarResultadosGrupo.php
@@ -6,25 +6,24 @@
     $conexion=conectarBD();
 
     $curso = $_GET['curso'];
-    // $dni = $_GET['dni'];
     $azul = 0;
     $rojo = 0;
     $verde = 0;
     $amarillo = 0;
-    echo "curso : ".$curso;
-    // echo "<br>dni : ".$dni;
 
-    $sql1="SELECT * FROM ALUMNO WHERE ALUMNO.id_curso =\"".$curso."\"";
-    echo "<br>".$sql1;
-    $consulta1=$conexion1->prepare($sql1);
+    echo"curso : ".$curso;
+    $sql1="SELECT ALUMNO.USUARIO_DNI as dni FROM ALUMNO WHERE ALUMNO.id_curso =\"".$curso."\"";
+    echo"<br>".$sql1;
+    $consulta1=$conexion->prepare($sql1);
     $consulta1->execute();
     $respuestas1=$consulta1->fetchAll();
 
+    print_r($respuestas1);
     for ($i=0; $i < count($respuestas1); $i++) {
 
         $sql="SELECT PREGUNTA.Enunciado, responde.RESPUESTA_Valor_Respuesta, ALUMNO.USUARIO_DNI, PREGUNTA.idPregunta from PREGUNTA, responde, ALUMNO 
-        WHERE responde.ALUMNO_USUARIO_DNI=ALUMNO.USUARIO_DNI and \"".$dni."\" = ALUMNO.USUARIO_DNI and PREGUNTA.idPregunta=responde.PREGUNTA_idPregunta";
-
+        WHERE responde.ALUMNO_USUARIO_DNI=ALUMNO.USUARIO_DNI and \"".$respuestas1[$i]->dni."\" = ALUMNO.USUARIO_DNI and PREGUNTA.idPregunta=responde.PREGUNTA_idPregunta";
+        echo"<br>".$sql;
         $consulta=$conexion->prepare($sql);
         $consulta->execute();
 
@@ -34,24 +33,23 @@
         $pregunta=$respuestas->Enunciado;
         $usuario=$respuestas->USUARIO_DNI;
         $respuesta=$respuestas->RESPUESTA_Valor_Respuesta;
-    
 
         for ($i=0; $i < count($respuestas); $i++) { 
             // echo $respuestas[$i]->Enunciado."-------".$respuestas[$i]->USUARIO_DNI."-------".$respuestas[$i]->RESPUESTA_Valor_Respuesta."<br>";
 
-            if ($dni == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 0 && $respuestas[$i]->idPregunta < 21) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
+            if ($dni[$i] == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 0 && $respuestas[$i]->idPregunta < 21) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
                 $azul += 1;
             }
     
-            if ($dni == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 20 && $respuestas[$i]->idPregunta < 41) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') { 
+            if ($dni[$i] == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 20 && $respuestas[$i]->idPregunta < 41) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') { 
                 $verde += 1;
             }
     
-            if ($dni == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 40 && $respuestas[$i]->idPregunta < 61) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
+            if ($dni[$i] == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 40 && $respuestas[$i]->idPregunta < 61) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
                 $rojo += 1;
             }
         
-            if ($dni == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 60 && $respuestas[$i]->idPregunta < 81) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
+            if ($dni[$i] == $respuestas[$i]->USUARIO_DNI && ($respuestas[$i]->idPregunta > 60 && $respuestas[$i]->idPregunta < 81) && $respuestas[$i]->RESPUESTA_Valor_Respuesta == 'VERDADERO') {
                 $amarillo += 1;
             }
     
