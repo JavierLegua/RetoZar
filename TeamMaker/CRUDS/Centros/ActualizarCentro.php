04bc6754c68ee1ce541d25970bc7332f9f094c43
Javier Legua <javierlegua14@gmail.com>
2021-11-30 12:40:55 +0100

    cambios

--- TeamMaker/CRUDS/Centros/ActualizarCentro.php
+++ TeamMaker/CRUDS/Centros/ActualizarCentro.php
@@ -4,31 +4,35 @@
 
     $conexion=conectarBD();
 
-    $nombre=$_POST['nombre'];
-    $idcentro=$_POST['idCentro'];
-    $direccion=$_POST['direccion'];
-
     $sql = "SELECT * FROM CENTRO";
 
     $consulta=$conexion->prepare($sql);
     $consulta->execute();
 
-    $centro=$consulta->fetch();
+    $centro=$consulta->fetchAll();
 
     $idCentroViejo=$centro->idCentro;
 
+    echo $idCentroViejo;
+
+    $nombre=$_POST['nombre'];
+    $idcentro=$_POST['idCentro'];
+    $direccion=$_POST['direccion'];
+
+   /*  echo $nombre;
+    echo $idcentro;
+    echo $direccion; */
 
     $sql2 = "UPDATE CENTRO SET idCentro=\"".$idcentro."\", nombre=\"".$nombre."\",Direccion =\"".$direccion."\" WHERE idCentro=\"".$idCentroViejo."\"";
-
 
     $consulta1=$conexion->prepare($sql2);
     $consulta1->execute(); 
     
     $nfilas=$consulta->rowCount();
 
-    if($nfilas==1){
+    /* if($nfilas==1){
         header("refresh:0.01;url=listarCentro?situacion=0");
     }else{
         header("refresh:0.01;url=listarCentro?situacion=1");
-    }
+    } */
     ?>
