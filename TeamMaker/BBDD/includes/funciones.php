dc78dfb892a251635dca3c8c5a24605eb76c747e
Javier Legua <javierlegua14@gmail.com>
2021-12-01 17:32:56 +0100

    cambios

--- TeamMaker/BBDD/includes/funciones.php
+++ TeamMaker/BBDD/includes/funciones.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="../Estilos/fonts.css">
-    <title>Document</title>
-    <script src="jquery-latest.js"></script>
-    <script src="Funciones.js"></script>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
    
     function conectarBD(){
