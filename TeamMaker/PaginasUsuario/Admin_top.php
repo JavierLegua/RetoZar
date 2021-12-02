7bfc192c391483c1ebc8a6acd743f14a93b52b50
Adelin <adebota02@gmail.com>
2021-12-02 11:35:04 +0100

    More changes

--- TeamMaker/PaginasUsuario/Admin_top.php
+++ TeamMaker/PaginasUsuario/Admin_top.php
@@ -7,7 +7,9 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="../Estilos/fonts.css">
+    <script src="../jquery-latest.js"></script>
     <link rel="stylesheet" href="../Estilos/Style.css">
     <title>Administrador supremo</title>
     <script src="../Funciones.js"></script>
